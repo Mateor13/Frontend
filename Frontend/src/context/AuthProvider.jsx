57db7904c53bb6442a6a7eeabf4831b5b480e519
Mateor13 <mateo.torres@epn.edu.ec>
2025-01-13 15:54:49 -0500

    Clase 13/1/2025

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -7,7 +7,7 @@ const AuthProvider = ({children}) => {
     const [auth, setAuth] = useState({})
     const perfil = async (token) => {
     try {
-        const url = `${import.meta.env.VITE_BACKEND_URL}/perfil/${token}`
+        const url = `${import.meta.env.VITE_BACKEND_URL}/perfil`
         const options = {
             headers:{
                 'Content-Type':'application/json',
@@ -22,15 +22,57 @@ const AuthProvider = ({children}) => {
 }
 useEffect(()=>{
     const token = localStorage.getItem('token')
-    if(token){
+    if(token)
+    {
         perfil(token)
     }
-})
+}, [])
+
+const actualizarPerfil = async(datos) => {
+    const token = localStorage.getItem('token')
+    try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`
+        const options = {
+            headers: {
+                method: 'PUT',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }
+        const respuesta = await axios.put(url, datos, options)
+        perfil(token)
+        return {respuesta:respuesta.data.msg,tipo:true}
+    } catch (error) {
+        return {respuesta:error.response.data.msg,tipo:false}
+    }
+}
+
+const actualizarPassword = async (datos) => {
+    const token = localStorage.getItem('token')
+    try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/actualizarpassword`
+        const options = {
+            headers: {
+                method: 'PUT',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }
+        const respuesta = await axios.put(url, datos, options)
+        return { respuesta: respuesta.data.msg, tipo: true }
+    } catch (error) {
+        return { respuesta: error.response.data.msg, tipo: false }
+    }
+}
+
+
     return <AuthContext.Provider value={
         {
             //Contenido del mensaje
             auth,
-            setAuth
+            setAuth,
+            actualizarPerfil,
+            actualizarPassword
         }
     }>
         {children}
--- Frontend/src/paginas/Perfil.jsx
+++ Frontend/src/paginas/Perfil.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { CardPerfil } from '../componets/Perfil/CardPerfil'
 import FormularioPerfil from '../componets/Perfil/FormularioPerfil'
+import Password from '../componets/Perfil/Password.jsx'
 
 const Perfil = () => {
     return (
         <>       
             <div>
-                <h1 className='font-black text-4xl text-gray-500'>Perfil</h1>
+                <h1 className='font-black text-4xl text-gray-500'>Perfil Veterinario</h1>
                 <hr className='my-4' />
-                <p className='mb-8'>Este módulo te permite visualizar el perfil del usuario......</p>
+                <p className='mb-8'>Este módulo te permite visualizar y actualizar el perfil del usuario </p>
             </div>
 
             <div className='flex justify-around gap-x-8 flex-wrap gap-y-8 md:flex-nowrap'>
@@ -17,6 +18,7 @@ const Perfil = () => {
                 </div>
                 <div className='w-full md:w-1/2'>
                     <CardPerfil/>
+                    <Password/>
                 </div>
             </div>
         </>
